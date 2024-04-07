ff5657f3d2222ba8d06db7f3d1521148b8d1e39f
ferewar <73423237+ferewar@users.noreply.github.com>
2024-04-07 09:25:58 -0700

    add dotenv file, add more robust regex email, removed uniqueness on name

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { authMiddleware } = require('./utils/auth');
 const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
